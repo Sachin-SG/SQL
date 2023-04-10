58cc13cb2f87073ba9558903d667adffb6a8b4e8
Sachin Golennavar <114339979+Sachin-SG@users.noreply.github.com>
2023-04-10 15:22:23 +0530

    Add files via upload

--- Counting NULL values in sql server.sql
+++ Counting NULL values in sql server.sql
@@ -43,3 +43,34 @@ select count(*) - count(CustomerID) from Ecommerce;
 SELECT count(*)  FROM Ecommerce WHERE CustomerID IS NULL 
 UNION ALL
 SELECT count(*) FROM Ecommerce WHERE CustomerID IS NOT NULL
+
+--Differene b/w Count(*) and  count(1) --------> No Difference
+
+SELECT count(*)  FROM Ecommerce
+SELECT count(1)  FROM Ecommerce
+SELECT count(-123)  FROM Ecommerce
+
+--Above 3  statements will give the same results ,the argument for the count function that we are providing is just used as
+-- value to assign the to each row by the count function for example -123 will be assigned to each row and the same will be counted  to give the result.
+
+--Differene b/w Count(column) and  count(1) --------> Yes there is a Difference
+SELECT count(*)  FROM Ecommerce -- Null values are also counted
+SELECT count(CustomerID)  FROM Ecommerce -- Null values are not counted
+
+--Countng null values from all columns
+select 
+  sum(case when CustomerID is null then 1 else 0 end) CustomerID_NULL_COUNT,
+  sum(case when Description is null then 1 else 0 end) Description_NULL_COUNT,
+  sum(case when InvoiceNo is null then 1 else 0 end) InvoiceNo_NULL_COUNT,
+  sum(case when StockCode is null then 1 else 0 end) StockCode_NULL_COUNT,
+  sum(case when Quantity is null then 1 else 0 end) Quantity_NULL_COUNT,
+  sum(case when UnitPrice is null then 1 else 0 end) UnitPrice_NULL_COUNT,
+  sum(case when InvoiceDate is null then 1 else 0 end) InvoiceDate_NULL_COUNT,
+  sum(case when Country is null then 1 else 0 end) Country_NULL_COUNT
+from Ecommerce;
+
+-- Replacing NULL values 
+Update Ecommerce
+set CustomerID = 'NA'
+where CustomerID is null; 
+
